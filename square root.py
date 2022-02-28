5faa1723df953d5a49d39ecc7545e4359203122a
Masai01 <98088765+Masai01@users.noreply.github.com>
2022-02-28 19:05:13 +0300

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,11 @@
+# Square.py
+#           A program that finds the square root of a number
+
+def main() :
+
+        num  =  int(input("Enter a number: "))
+        result  =  num**0.5
+
+        print("The square of", num, "is" ,  result)
+
+main()
